94786f73c5eee32177eaf8a1f2de1fc3176c1221
Lara Schmidt <lara.schmidt205@gmail.com>
2025-02-15 19:36:20 +0100

    Clear workspace and add files from task 2A

---
+++ preclass_assignment/main.py
@@ -0,0 +1,22 @@
+# Main: excecution of functions
+
+# Import functions
+from functions import *
+
+# Ex 1
+print(greet('world'))
+
+# Ex 2
+print(goldilocks(139))
+print(goldilocks(140))
+print(goldilocks(151))
+print(goldilocks(150))
+
+# Ex 3
+print(square_list([1, 2, 3]))
+
+#Ex 4
+print(fibonacci_stop(30))
+
+#Ex 5
+print(clean_pitch([-1, 2, 6, 95], [1, 0, 0, 0]))
