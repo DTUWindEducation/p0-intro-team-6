0de1fc5ecd1cec0ea425db112275ae822c6d5bcd
s250244 <s250244@student.dtu.dk>
2025-02-15 13:32:44 +0100

    week2 part 2A: deleted double files; creation of main.py, function.py and FunctionDiagram.py

---
+++ preclass_assignment/function.py
@@ -0,0 +1,50 @@
+# Function for week 2
+def greet(name):
+    """Returns in terminal a greet to the given name"""
+    return print(f"Hello, {name}!")
+
+def goldilocks(bed_size):
+    """define if the bed_size is good for goldilocks standarts"""
+    if bed_size < 140:
+        print("Too small!")
+    elif bed_size > 150:
+        print("Too large!")
+    else:
+        print("Just right. :)")
+
+def square_list(list):
+    """Takes in a list a give back a list with each element squared"""
+    new_list=[]
+    for i in list:
+        new_list.append(i**2)
+    return new_list
+
+def fibonacci_stop(max_val):
+    """give the element of the fibonacci sequence lower that the max_val"""
+    fib_list = []
+    i = 1
+    if max_val < 1:
+        return fib_list
+    fib_list.extend([1,1])
+    i = 2
+    while i < max_val:
+        fib_list.append(i)
+        i = fib_list[-2] + fib_list [-1]
+        
+    return fib_list
+
+def clean_pich(measurement, status):
+    """Clean the measurement depending on the status signal list"""
+
+    if len(measurement)!=len(status):
+        raise Exception("Not the same lenght of signal !")
+    
+    empty_measurement = measurement[:]
+
+    for i in range(len(measurement)):
+        if status[i] == 1:
+            empty_measurement[i] = -999
+    return empty_measurement
+
+
+    
---
+++ preclass_assignment/main.py
@@ -0,0 +1,43 @@
+###Testiong the function
+from function import *
+import random
+
+#Testing greet
+greet("Donald Duck")
+
+#Testing goldilock
+too_big = 151
+too_small = 139
+just_right = 145
+
+print(f"The result for {too_big} is : ", end='')
+goldilocks(too_big)
+
+print(f"The result for {too_small} is : ",end='')
+goldilocks(too_small)
+
+print(f"The result for {just_right} is : ", end="")
+goldilocks(just_right)
+
+#testing square list
+list_to_square = list(range(1,20,3))
+print(f"Intial list: {list_to_square}")
+print(f"Squared list: {square_list(list_to_square)}")
+
+#testing fibonacci_stop
+max_nb = 16.49
+print(f"for the max={max_nb} the list is: {fibonacci_stop(max_nb)}")
+
+low_nb = -5
+print(f"for the max={low_nb} the list is: {fibonacci_stop(low_nb)}")
+
+exact_nb = 13
+print(f"for the max={exact_nb} the list is: {fibonacci_stop(exact_nb)}")
+
+#test of clean_pitch
+rd_measurement = random.sample(range(-2,93), 10)
+rd_signal = random.choices([0,1], k=10)
+
+print(f"For measurement: {rd_measurement}")
+print(f"and theses signal status: {rd_signal}")
+print(f"The cleaned picth is {clean_pich(measurement=rd_measurement, status=rd_signal)}")
